8b76c31dff6d307de8b9c47f30f6ff8cb9f2d9cc
Watchout140 <46763263+Watchout140@users.noreply.github.com>
2023-05-08 14:54:25 +0200

    error handling

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,13 +3,10 @@ import classes from "./Board.module.css";
 import { DragDropContext } from "react-beautiful-dnd";
 import Column from "./Column";
 import AddTeam from "../Teams/AddTeam";
+
 const reorderColumnList = (sourceCol, startIndex, endIndex) => {
-  console.log("STARTINDEX", startIndex);
-  console.log("ENDINDEX", endIndex);
-  console.log("SOURCECOL", Array.from(sourceCol.userIds));
   const newUserIds = Array.from(sourceCol.userIds);
   const [removed] = newUserIds.splice(startIndex, 1);
-
   newUserIds.splice(endIndex, 0, removed);
 
   const newColumn = {
@@ -18,6 +15,23 @@ const reorderColumnList = (sourceCol, startIndex, endIndex) => {
   };
   return newColumn;
 };
+
+function getDifference(dataTeams1Map, dataTeams2Map) {
+  const difference = [];
+  for (const [teamName, team] of dataTeams2Map) {
+    if (!dataTeams1Map.has(teamName)) {
+      difference.push(team);
+    } else {
+      const team1 = dataTeams1Map.get(teamName);
+
+      if (JSON.stringify(team1.userIds) !== JSON.stringify(team.userIds)) {
+        difference.push(team);
+      }
+    }
+  }
+  return difference;
+}
+
 function Board({
   users,
   dataTeams,
@@ -35,16 +49,13 @@ function Board({
       columnOrder: newAddedColumnOrder,
     });
   }
-  console.log("hej", dataTeams);
 
   const columnOrder = Object.keys(dataTeams);
-  console.log("columnOrder", columnOrder);
   const initialData = { users, dataTeams, columnOrder };
-
-  console.log("InitialData", initialData);
-
   const [originalState, setOriginalState] = useState([]);
   const [state, setState] = useState(initialData);
+  const mousePosition = { x: 0, y: 0 };
+  var pinnedTeams = [];
 
   function transformDataTeams(state) {
     if (Array.isArray(state.dataTeams)) {
@@ -62,58 +73,34 @@ function Board({
       dataTeams: dataTeamsArray,
     };
   }
+  function show() {
+  }
   function handleSave() {
     const transformedState = transformDataTeams(state);
-    console.log("SAVEDDATA", transformedState);
     const dataTeams1Map = new Map(
       originalState.dataTeams.map((team) => [team.teamName, team])
     );
     const dataTeams2Map = new Map(
       transformedState.dataTeams.map((team) => [team.teamName, team])
     );
-    console.log("ORIGINAL", originalState);
 
-    const difference = [];
-    const deletedDifference = [];
-    for (const [teamName, team] of dataTeams2Map) {
-      if (!dataTeams1Map.has(teamName)) {
-        difference.push(team);
-      } else {
-        const team1 = dataTeams1Map.get(teamName);
+    const difference = getDifference(dataTeams1Map, dataTeams2Map);
+    const deletedDifference = getDifference(dataTeams2Map, dataTeams1Map);
 
-        if (JSON.stringify(team1.userIds) !== JSON.stringify(team.userIds)) {
-          difference.push(team);
-        }
-      }
-    }
-    for (const [teamName, team] of dataTeams1Map) {
-      if (!dataTeams2Map.has(teamName)) {
-        deletedDifference.push(team);
-      } else {
-        const team1 = dataTeams2Map.get(teamName);
-
-        if (JSON.stringify(team1.userIds) !== JSON.stringify(team.userIds)) {
-          deletedDifference.push(team);
-        }
-      }
-    }
     const output = {
       dataTeams: difference,
     };
     const output2 = {
       dataTeams: deletedDifference,
     };
-    console.log("SAVEDDATA", output);
-    console.log("SAVEDDATA2", output2);
+
     handleSaved(output, output2);
   }
   function handleStart() {
-    console.log("HANDLESTARTINITAILDATA", initialData);
     const copy = { ...initialData };
     setOriginalState(copy);
     setState(initialData);
   }
-  console.log("STATE", state);
   function handleDelete(teamName) {
     const updatedDataTeams = deleteTeam(teamName);
     const newColumnOrder = Object.keys(updatedDataTeams);
@@ -136,225 +123,6 @@ function Board({
     setState({ ...state, dataTeams: newDataTeams });
   };
 
-  const onDragEnd = (result) => {
-    console.log(result);
-    const { destination, source } = result;
-    if (!destination) {
-      return;
-    }
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    ) {
-      return;
-    }
-
-    const sourceCol =
-      state.dataTeams[
-        dataTeams.findIndex((team) => team.teamName === source.droppableId)
-      ];
-
-    const destinationCol =
-      state.dataTeams[
-        dataTeams.findIndex((team) => team.teamName === destination.droppableId)
-      ];
-
-    if (sourceCol.teamName === destinationCol.teamName) {
-      const newColumn = reorderColumnList(
-        sourceCol,
-        source.index,
-        destination.index
-      );
-
-      const newState = {
-        ...state,
-        dataTeams: {
-          ...state.dataTeams,
-          [dataTeams.findIndex((team) => team.teamName === newColumn.teamName)]:
-            newColumn,
-        },
-      };
-
-      setState(newState);
-      return;
-    }
-    console.log("SOURCECOLLLLLL", sourceCol);
-    if (sourceCol.teamName === "Users") {
-      const startUserIds = Array.from(sourceCol.userIds);
-
-      console.log("STARTUSERIDS", startUserIds);
-
-      const [removed] = startUserIds.splice(source.index, 1);
-      const objectToAdd = {
-        username: removed,
-        teamName: "Users",
-        availability: 0,
-        role: [],
-      };
-      console.log("OBJECTTOADD", objectToAdd);
-      console.log("REMOVED", [removed]);
-      console.log("STARTUSERIDS", startUserIds);
-      const newStartCol = {
-        ...sourceCol,
-      };
-
-      console.log("NEWSTARTCOL", newStartCol);
-      const endUserIds = Array.from(destinationCol.userIds);
-      const endMetaData = Array.from(destinationCol.metaData);
-      console.log("ENDUSERIDS", endUserIds);
-      endUserIds.splice(destination.index, 0, removed);
-      objectToAdd.teamName = destinationCol.teamName;
-      endMetaData.push(objectToAdd);
-      console.log("DESTINDEX", destination.index);
-      console.log("ENDUSERIDS", endUserIds);
-      const newEndCol = {
-        ...destinationCol,
-        userIds: endUserIds,
-        metaData: endMetaData,
-      };
-      console.log("NEWENDCOL", newEndCol);
-      const newDataTeams = {
-        dataTeams: {
-          ...state.dataTeams,
-          [dataTeams.findIndex(
-            (team) => team.teamName === newStartCol.teamName
-          )]: newStartCol,
-          [dataTeams.findIndex((team) => team.teamName === newEndCol.teamName)]:
-            newEndCol,
-        },
-      };
-      console.log("NEWDATATEAMS", newDataTeams);
-      setDataTeams(newDataTeams);
-      console.log("NEWSTATE", newDataTeams);
-      const newState = {
-        ...state,
-        dataTeams: {
-          ...state.dataTeams,
-          [dataTeams.findIndex(
-            (team) => team.teamName === newStartCol.teamName
-          )]: newStartCol,
-          [dataTeams.findIndex((team) => team.teamName === newEndCol.teamName)]:
-            newEndCol,
-        },
-      };
-      console.log("NEWSTATE", newState);
-      setState(newState);
-    } else {
-      const startUserIds = Array.from(sourceCol.userIds);
-      const startMetaData = Array.from(sourceCol.metaData);
-      console.log("STARTMETADATA", startMetaData);
-      console.log("STARTUSERIDS", startUserIds);
-
-      const [removed] = startUserIds.splice(source.index, 1);
-      const objectToAdd = startMetaData.find(
-        (item) => item.username === removed
-      );
-      console.log("OBJECTTOADD", objectToAdd);
-      const newArray = startMetaData.filter(
-        (item) => item.username !== removed
-      );
-      console.log("REMOVED", [removed]);
-      console.log("NEWARRAY", newArray);
-
-      const newStartCol = {
-        ...sourceCol,
-        userIds: startUserIds,
-        metaData: newArray,
-      };
-
-      if (destinationCol.teamName === "Users") {
-        console.log("DESTINATIONCOLTEAMNAME", destinationCol.teamName);
-        console.log("NEWSTARTCOL", newStartCol);
-        const endUserIds = Array.from(destinationCol.userIds);
-        const newEndUserIds = Array.from(destinationCol.userIds);
-        console.log("ENDUSERIDS", endUserIds);
-        endUserIds.splice(destination.index, 0, removed);
-        console.log("DESTINDEX", destination.index);
-        console.log("ENDUSERIDS", endUserIds);
-        const newEndCol = {
-          ...destinationCol,
-          userIds: newEndUserIds,
-        };
-        console.log("NEWENDCOL", newEndCol);
-        const newDataTeams = {
-          dataTeams: {
-            ...state.dataTeams,
-            [dataTeams.findIndex(
-              (team) => team.teamName === newStartCol.teamName
-            )]: newStartCol,
-            [dataTeams.findIndex(
-              (team) => team.teamName === newEndCol.teamName
-            )]: newEndCol,
-          },
-        };
-        console.log("NEWDATATEAMS", newDataTeams);
-        setDataTeams(newDataTeams);
-        console.log("NEWSTATE", newDataTeams);
-        const newState = {
-          ...state,
-          dataTeams: {
-            ...state.dataTeams,
-            [dataTeams.findIndex(
-              (team) => team.teamName === newStartCol.teamName
-            )]: newStartCol,
-            [dataTeams.findIndex(
-              (team) => team.teamName === newEndCol.teamName
-            )]: newEndCol,
-          },
-        };
-        console.log("NEWSTATE", newState);
-        setState(newState);
-      } else {
-        console.log("DESTINATIONCOLTEAMNAME", destinationCol.teamName);
-        console.log("NEWSTARTCOL", newStartCol);
-        const endUserIds = Array.from(destinationCol.userIds);
-        const endMetaData = Array.from(destinationCol.metaData);
-        console.log("ENDUSERIDS", endUserIds);
-        console.log("ENDUSERIDSMEETDATA", endMetaData);
-        endUserIds.splice(destination.index, 0, removed);
-        objectToAdd.teamName = destinationCol.teamName;
-        endMetaData.push(objectToAdd);
-        console.log("METADATAEND", endMetaData);
-        console.log("DESTINDEX", destination.index);
-        console.log("ENDUSERIDS", endUserIds);
-        const newEndCol = {
-          ...destinationCol,
-          userIds: endUserIds,
-          metaData: endMetaData,
-        };
-        console.log("NEWENDCOL", newEndCol);
-        const newDataTeams = {
-          dataTeams: {
-            ...state.dataTeams,
-            [dataTeams.findIndex(
-              (team) => team.teamName === newStartCol.teamName
-            )]: newStartCol,
-            [dataTeams.findIndex(
-              (team) => team.teamName === newEndCol.teamName
-            )]: newEndCol,
-          },
-        };
-        console.log("NEWDATATEAMS", newDataTeams);
-        setDataTeams(newDataTeams);
-        console.log("NEWSTATE", newDataTeams);
-        const newState = {
-          ...state,
-          dataTeams: {
-            ...state.dataTeams,
-            [dataTeams.findIndex(
-              (team) => team.teamName === newStartCol.teamName
-            )]: newStartCol,
-            [dataTeams.findIndex(
-              (team) => team.teamName === newEndCol.teamName
-            )]: newEndCol,
-          },
-        };
-        console.log("NEWSTATE", newState);
-        setState(newState);
-      }
-    }
-  };
-  console.log("STATATDTATDTATD", state);
   function mergeMetaData(metaData, users) {
     var newArray = [];
     if (!Array.isArray(metaData)) {
@@ -374,13 +142,30 @@ function Board({
       return updatedUser;
     });
   }
+
+  function togglePinnedTeam(teamName) {
+    pinnedTeams = [];
+    if(state.pinnedTeams !== undefined) pinnedTeams = state.pinnedTeams;
+    const teamId = Object.values(state.dataTeams).findIndex((team) => team.teamName === teamName);
+    const deleteList = pinnedTeams.filter((item) => item === teamId);
+    if(deleteList.length == 0) pinnedTeams.push(teamId);
+    else pinnedTeams = pinnedTeams.filter((item) => item !== teamId);
+    var newColumnOrder = ["0"];
+    pinnedTeams.map(item => newColumnOrder.push(item + ''));
+    newColumnOrder.push(...state.columnOrder.filter((teamId) => !newColumnOrder.includes(teamId)));
+    const newState = {
+      ...state,
+      columnOrder: newColumnOrder,
+      pinnedTeams: pinnedTeams
+    };
+    setState(newState);
+  }
+
   function changeAvailability(name, teamName, availability) {
-    console.log("CHANGEDATATEAM", state.dataTeams);
     const dataTeamsArray = Object.values(state.dataTeams);
     const index = dataTeamsArray.findIndex(
       (team) => team.teamName === teamName
     );
-    console.log("INDEX", index);
     const intAvailability = parseInt(availability);
     const changeDataTeam = dataTeamsArray[index];
 
@@ -397,7 +182,6 @@ function Board({
       state.dataTeams = Object.values(state.dataTeams);
     //const dataTeamsArray = Object.values(state.dataTeams);
 
-    console.log("DataTeams: ", state.dataTeams);
     const newState = {
       ...state,
       dataTeams: [
@@ -409,64 +193,139 @@ function Board({
 
     setState(newState);
   }
+
+  function onDragStart(result) {
+    if(result.source.droppableId == "Users") return;
+    const memberElements = document.querySelectorAll('[data-rbd-draggable-id="' + result.draggableId + '"]');
+    if(memberElements.length == 0) return;
+    const memberElement = memberElements[0];
+    const x = Number(memberElement.style.left.replaceAll("px", ""));
+    const left = x - (220 - (mousePosition.x - x));
+    const y = Number(memberElement.style.left.replaceAll("px", ""));
+    const top = y - (45 - (mousePosition.y - y));
+    memberElement.style.left = left + "px";
+    memberElement.style.top = top + "px";
+  }
+
+  function onPersonClick(e) {
+    mousePosition.x = e.clientX;
+    mousePosition.y = e.clientY;
+  }
+
+  const onDragEnd = (result) => {
+    const { destination, source } = result;
+    if (!destination) {
+      return;
+    }
+    if (destination.droppableId === source.droppableId && destination.index === source.index) {
+      return;
+    }
+  
+    const sourceCol = state.dataTeams[dataTeams.findIndex((team) => team.teamName === source.droppableId)];
+    const destinationCol = state.dataTeams[dataTeams.findIndex((team) => team.teamName === destination.droppableId)];
+  
+    const movedMemberName = result.draggableId.replaceAll(result.source.droppableId, "");
+    if (destinationCol.userIds.includes(movedMemberName)) return;
+  
+    const startUserIds = Array.from(sourceCol.userIds);
+    const [removed] = startUserIds.splice(source.index, 1);
+  
+    const endUserIds = Array.from(destinationCol.userIds);
+    endUserIds.splice(destination.index, 0, removed);
+  
+    const updateState = (newStartCol, newEndCol) => {
+      const newState = {
+        ...state,
+        dataTeams: {
+          ...state.dataTeams,
+          [dataTeams.findIndex((team) => team.teamName === newStartCol.teamName)]: newStartCol,
+          [dataTeams.findIndex((team) => team.teamName === newEndCol.teamName)]: newEndCol,
+        },
+      };
+      setState(newState);
+    };
+  
+    if (sourceCol.teamName === destinationCol.teamName) {
+      const newColumn = reorderColumnList(sourceCol, source.index, destination.index);
+      updateState(newColumn, newColumn);
+      return;
+    }
+  
+    const startMetaData = Array.from(sourceCol.metaData);
+    const objectToAdd = sourceCol.teamName === "Users" ? {
+      username: removed,
+      teamName: "Users",
+      availability: 0,
+      role: [],
+    } : startMetaData.find((item) => item.username === removed);
+  
+    const newStartCol = {
+      ...sourceCol,
+      userIds: startUserIds,
+      metaData: startMetaData.filter((item) => item.username !== removed),
+    };
+  
+    const endMetaData = Array.from(destinationCol.metaData);
+    const newEndCol = {
+      ...destinationCol,
+      userIds: endUserIds,
+      metaData: destinationCol.teamName === "Users" ? endMetaData : [...endMetaData, { ...objectToAdd, teamName: destinationCol.teamName }],
+    };
+  
+    updateState(newStartCol, newEndCol);
+  
+    const newDataTeams = {
+      dataTeams: {
+        ...state.dataTeams,
+        [dataTeams.findIndex((team) => team.teamName === newStartCol.teamName)]: newStartCol,
+        [dataTeams.findIndex((team) => team.teamName === newEndCol.teamName)]: newEndCol,
+      },
+    };
+    setDataTeams(newDataTeams);
+  };  
+
+  const renderColumns = (teamIdCondition, additionalProps = {}) => {
+    return state.columnOrder.map((teamId) => {
+      if (teamIdCondition(teamId)) {
+        const column = state.dataTeams[teamId];
+        const userss = column.userIds.map(
+          (userId) => state.users[state.users.findIndex((user) => user.username === userId)]
+        );
+  
+        const newUsers = teamId !== "0" ? mergeMetaData(column.metaData, userss) : userss;
+  
+        return (
+          <Column
+            key={column.teamName}
+            column={column}
+            users={newUsers}
+            onClose={handleDelete}
+            {...additionalProps}
+          />
+        );
+      }
+      return null;
+    });
+  };
+  
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
+    <DragDropContext onDragStart={onDragStart} onDragEnd={onDragEnd}>
       <div className={classes.container}>
         <div className={classes.addTeam}>
           <AddTeam onAddTeam={handelAddTeam} />
           <button onClick={handleStart}>getData</button>
           <button onClick={handleSave}>saveData</button>
+          <button onClick={show}>Show Order</button>
         </div>
         <div className={classes.columns}>
           <div className={classes.userColumn}>
-            {state.columnOrder.map((teamId) => {
-              if (teamId === "0") {
-                const column = state.dataTeams[teamId];
-
-                const userss = column.userIds.map(
-                  (userId) =>
-                    state.users[
-                      state.users.findIndex((user) => user.username === userId)
-                    ]
-                );
-
-                return (
-                  <Column
-                    key={column.teamName}
-                    column={column}
-                    users={userss}
-                    onClose={handleDelete}
-                  />
-                );
-              }
-              return null;
-            })}
+            {renderColumns((teamId) => teamId === "0")}
           </div>
-          <div className={classes.teamsColumn}>
-            {state.columnOrder.map((teamId) => {
-              if (teamId !== "0") {
-                const column = state.dataTeams[teamId];
-                const userss = column.userIds.map(
-                  (userId) =>
-                    state.users[
-                      state.users.findIndex((user) => user.username === userId)
-                    ]
-                );
-
-                const newUsers = mergeMetaData(column.metaData, userss);
-
-                return (
-                  <Column
-                    key={column.teamName}
-                    column={column}
-                    users={newUsers}
-                    onClose={handleDelete}
-                    state={state}
-                    changeAvailability={changeAvailability}
-                  />
-                );
-              }
-              return null;
+          <div className={classes.teamsColumn} onMouseDown={onPersonClick}>
+            {renderColumns((teamId) => teamId !== "0", {
+              state,
+              changeAvailability,
+              togglePinnedTeam
             })}
           </div>
         </div>
--- src/components/Board/Column.js
+++ src/components/Board/Column.js
@@ -1,89 +1,72 @@
 import classes from "./Column.module.css";
 import { Draggable, Droppable } from "react-beautiful-dnd";
 import { toast, Toaster } from "react-hot-toast";
+import pin_selected from "../../assets/pin_selected.png";
+import pin_unselected from "../../assets/pin_unselected.png";
 
-function Column({ column, users, onClose, changeAvailability }) {
+function Column({ column, users, onClose, changeAvailability, togglePinnedTeam }) {
+  const isUsersColumn = column.teamName === "Users";
+
   function handleClick() {
     toast.success("Deleted " + column.teamName);
     onClose(column.teamName);
   }
 
   function handleAvailabilityChange(e, user, teamName) {
+    if (e.target.value > 100) {
+      e.target.value = 100;
+      e.preventDefault();
+    } else if (e.target.value < 0) {
+      e.target.value = 0;
+      e.preventDefault();
+    }
     if (e.key === "Enter") {
-      if (e.target.value > 100) e.target.value = 100;
-      else if (e.target.value < 0) e.target.value = 0;
-      console.log("CHANGE", user.username);
-      console.log("CHANGE", e.target.value);
-      console.log("CHANGE", teamName);
       changeAvailability(user.username, teamName, e.target.value);
     }
   }
 
-  const userLength = Array.from(users);
+  function handleTogglePinnedTeam(e) {
+    if(e.target.nodeName == 'A') return;
+    const a = e.target.parentElement, img = e.target;
+    const isPinned = !a.classList.contains(classes.pinned_wrapper_team);
+    a.classList.toggle(classes.pinned_wrapper_team, isPinned);
+    img.src = isPinned ? pin_selected : pin_unselected;
+    togglePinnedTeam(column.teamName);
+  }
+
+  const userLength = users.length;
 
   return (
-    <div
-      className={`${classes.column} ${
-        column.teamName === "Users" ? classes.userCol : ""
-      }`}
-    >
-      <Toaster />
-      <div
-        className={`${classes.header} ${
-          column.teamName === "Users" ? classes.userHeader : ""
-        }`}
-      >
-        <p className={classes.count}>{userLength.length}</p>
-        <p
-          className={`${classes.title} ${
-            column.teamName === "Users" ? classes.userTitle : ""
-          }`}
-        >
+    <div className={`${classes.column} ${isUsersColumn ? classes.userCol : ""}`}>
+      <Toaster/>
+      <div className={`${classes.header} ${isUsersColumn ? classes.userHeader : ""}`}>
+        <p className={classes.count}>
+          <a onClick={handleTogglePinnedTeam} className={classes.pin_wrapper}>
+            <img src={pin_unselected} className={classes.pin}></img>
+          </a>
+          <span>{userLength}</span>
+        </p>
+        <p className={`${classes.title} ${isUsersColumn ? classes.userTitle : ""}`}>
           {column.teamName}
         </p>
-
-        <span
-          className={`${classes.close} ${
-            column.teamName === "Users" ? classes.noClose : ""
-          }`}
-          onClick={() => handleClick(column.teamName)}
-        >
+        <span className={`${classes.close} ${isUsersColumn ? classes.noClose : ""}`} onClick={handleClick}>
           &times;
         </span>
       </div>
       <Droppable droppableId={`${column.teamName}`}>
         {(provided, snapshot) => (
-          <div
-            className={`${classes.list} ${
-              column.teamName === "Users" ? classes.userList : ""
-            }`}
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
+          <div className={`${classes.list} ${isUsersColumn ? classes.userList : ""}`} ref={provided.innerRef} {...provided.droppableProps}>
             {users.map((user, index) => (
-              <Draggable
-                key={user.username + column.teamName}
-                draggableId={`${user.username + column.teamName}`}
-                index={index}
-              >
+              <Draggable key={user.username + column.teamName} draggableId={`${user.username + column.teamName}`} index={index}>
                 {(provided, snapshot) => (
-                  <div
-                    className={`${classes.users} ${
-                      snapshot.isDragging ? classes.dragging : ""
-                    }`}
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
+                  <div className={`${classes.users} ${snapshot.isDragging ? classes.dragging : ""}`} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
                     <div className={classes.info}>
                       <p>
                         {user.firstname} {user.lastname}
                       </p>
                       <p className={classes.hidden}>
-                        {true ? user.username : ""}
-                        <br></br>
-                        {true ? user.role : ""}
-                        <br></br>
+                        {user.username}<br />
+                        {user.role}<br />
                         <span className={classes.rt_input}>
                           <input
                             name="availability"
@@ -92,11 +75,8 @@ function Column({ column, users, onClose, changeAvailability }) {
                             min="0"
                             max="100"
                             defaultValue={user.availability}
-                            onKeyPress={(e) =>
-                              handleAvailabilityChange(e, user, column.teamName)
-                            }
-                          />
-                          %
+                            onKeyPress={(e) => handleAvailabilityChange(e, user, column.teamName)}
+                          />%
                         </span>
                       </p>
                     </div>
@@ -104,6 +84,7 @@ function Column({ column, users, onClose, changeAvailability }) {
                 )}
               </Draggable>
             ))}
+            {provided.placeholder}
           </div>
         )}
       </Droppable>
